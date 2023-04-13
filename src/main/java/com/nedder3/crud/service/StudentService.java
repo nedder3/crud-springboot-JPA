1f641b9253640a85b316ea26c38132f8e2abec01
nedder3 <arij.dev@gmail.com>
2023-04-13 07:59:07 -0300

    ADD crud methods

--- src/main/java/com/nedder3/crud/service/StudentService.java
+++ src/main/java/com/nedder3/crud/service/StudentService.java
@@ -1,4 +1,41 @@
 package com.nedder3.crud.service;
 
+import com.nedder3.crud.model.Student;
+import com.nedder3.crud.repository.IStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+    @Autowired
+    private IStudentRepository iStudentRepository;
+
+    public Student saveStudent(Student student){
+       if (student.getId() == null){
+        return iStudentRepository.save(student);
+       }
+        return  null;
+    }
+
+    public Page<Student> getAllStudents(Integer page, Integer size,boolean enablePagination){
+
+        return iStudentRepository.findAll(enablePagination ? PageRequest.of(page,size): Pageable.unpaged());
+    }
+
+    public void deleteStudent(Long id){
+        iStudentRepository.deleteById(id);
+
+    }
+
+    public Student editStudent(Student student){
+       if (student.getId() !=null && iStudentRepository.existsById(student.getId())){
+           return iStudentRepository.save(student);
+       }
+
+        return null;
+    }
+
 }
